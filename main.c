5a70e1cf405560730ab05a0c97a950795dc9f532
Santhosh <131981850+Black-Santhosh@users.noreply.github.com>
2025-08-23 01:52:08 +0000

    Function types

--- main.c
+++ main.c
@@ -1,6 +1,65 @@
 #include<stdio.h>
 
+/*
+// 1. without argument without return
+
+void add()
+{
+    int a = 10, b = 5;
+    int c = a+b;
+    printf("%d\n",c);
+}
+
 void main()
 {
-    printf("Hello\n");
+    add();
 }
+*/
+
+/*
+// without argument with return
+
+int add()
+{
+    int a = 10, b = 5;
+    int c = a+b;
+    return c;
+}
+
+void main()
+{
+    int d = add();
+    printf("%d\n",d);
+}
+    */
+
+
+    /*
+// with argument with return
+
+int add(int a, int b)
+{
+    int c = a+b;
+    return c;
+}
+
+void main()
+{
+    int d = add(10,5);
+    printf("%d\n",d);
+}    
+*/
+
+
+// with argument without return
+
+void add(int a, int b)
+{
+    int c = a+b;
+    printf("%d\n",c);
+}
+
+void main()
+{
+    add(10,5);
+}  
